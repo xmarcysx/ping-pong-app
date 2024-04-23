ab7419e56d97a60b1bcc832e602aa9db51a4991e
Marcysiox <marcinchowaniec21@gmail.com>
2024-04-23 21:18:19 +0200

    Sekcja uzytkownika

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [RouterOutlet, ButtonModule, MenuComponent],
 })
 export class AppComponent {
   title = 'ping-pong-app';
