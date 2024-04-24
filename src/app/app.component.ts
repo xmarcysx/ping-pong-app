4c26791933ba4fbabdb0d7fbf0144eac75634247
Marcysiox <marcinchowaniec21@gmail.com>
2024-04-24 20:39:03 +0200

    Side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { MenuComponent } from './shared/menu/menu.component';
+import { MainPageComponent } from './shared/main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, ButtonModule, MenuComponent],
+  imports: [RouterOutlet, ButtonModule, MenuComponent, MainPageComponent],
 })
 export class AppComponent {
   title = 'ping-pong-app';
